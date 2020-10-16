cc908f2d1acf231c210e59a93acd1a718d12cfb9
alebelcor <856838+alebelcor@users.noreply.github.com>
2020-10-16 12:09:51 -0700

    feat: Add early domain resolution for thumbnail URLs

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,9 +1,19 @@
 /* eslint-disable react/prop-types */
 
+import Head from 'next/head';
+
 import 'src/styles/index.css';
 
 const AppForReddit = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="dns-prefetch" href="https://a.thumbs.redditmedia.com" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default AppForReddit;
